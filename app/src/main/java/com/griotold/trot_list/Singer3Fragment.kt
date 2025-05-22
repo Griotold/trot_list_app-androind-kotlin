a47486f0454d48f0354107b27cbf88a028dd28cd
Griotold <gotjd9773@naver.com>
2025-05-22 16:04:24 +0900

    3. 리사이클러 뷰 활용해서 노래 제목 리스트 출력하기

--- app/src/main/java/com/griotold/trot_list/Singer3Fragment.kt
+++ app/src/main/java/com/griotold/trot_list/Singer3Fragment.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class Singer3Fragment : Fragment() {
 
@@ -20,6 +22,31 @@ class Singer3Fragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_singer3, container, false)
+
+        val items = mutableListOf<String>()
+        items.add("가인이여라")
+        items.add("진정인가요")
+        items.add("당신을 만나")
+        items.add("가인이여라")
+        items.add("진정인가요")
+        items.add("당신을 만나")
+        items.add("가인이여라")
+        items.add("진정인가요")
+        items.add("당신을 만나")
+        items.add("가인이여라")
+        items.add("진정인가요")
+        items.add("당신을 만나")
+        items.add("가인이여라")
+        items.add("진정인가요")
+        items.add("당신을 만나")
+
+
+        val rv = view.findViewById<RecyclerView>(R.id.singRV)
+        val rvAdapter = RVAdapter(items)
+
+        rv.adapter = rvAdapter
+
+        rv.layoutManager = LinearLayoutManager(context)
 
         view.findViewById<ImageView>(R.id.image1).setOnClickListener {
             it.findNavController().navigate(R.id.action_singer3Fragment_to_singer1Fragment)
